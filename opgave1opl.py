9ef0305d34398f3a5f56025346f1ba7477c1b1e8
Johan De Deyne <jdedeyne@gmail.com>
2021-11-04 19:53:24 +0100

    venv

--- opgave1opl.py
+++ opgave1opl.py
@@ -4,8 +4,37 @@ from math import *
 #   len(array)
 #   nan (vanaf Python 3.5)
 
-def ggd(x, y):
-    return x + y
+def ggd(getal1, getal2):
+    rest = 1
+    grootste = 0
+    kleinste = 0
+
+    if getal1 > getal2:
+        grootste=getal1
+        kleinste=getal2
+    else:
+        grootste=getal2
+        kleinste=getal1
+
+    while rest != 0:    
+        rest = grootste%kleinste
+    if rest != 0:
+        grootste=kleinste
+        kleinste=rest
+    
+    return kleinste
 
 def keerOm(x):
     return x
+
+def faculteit(x):
+    return x
+
+def gemiddelde(x):
+    return None
+
+def grootstePriemKleinerDanOfGelijkAan(x):
+    return None
+
+def variatie(x):
+    return None
