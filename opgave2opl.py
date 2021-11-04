e2310b91c1a170d2c742ecb432ddf5b6ce206115
Johan De Deyne <jdedeyne@gmail.com>
2021-11-04 20:44:07 +0100

    opgave2

---
+++ opgave2opl.py
@@ -0,0 +1,14 @@
+# toegelaten
+#   ord() en chr()
+#   array.append()
+#   len(array)
+#   len(string)
+#   string[index]
+#   onderstaande functie isWhitespace(ch)
+
+def split(str, sep):
+    woorden = []
+    return woorden
+
+def isWhitespace(ch):
+    return ch == ' ' or ch == '\t' or ch == '\r' or ch == '\n'
