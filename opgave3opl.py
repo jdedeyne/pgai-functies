695b9aa5cee89c4aff473759af577ce4ad83a89b
Johan De Deyne <jdedeyne@gmail.com>
2021-11-11 17:43:23 +0100

    opgave3

---
+++ opgave3opl.py
@@ -0,0 +1,29 @@
+# toegelaten
+#   array.append()
+#   len(array)
+#   len(string)
+#   string[index]
+
+def substr(str, start, end):
+    return NotImplemented
+
+def reverse(str):
+    return NotImplemented
+
+def isPalindrome(str):
+    return NotImplemented
+
+def find(str, instr, index):
+    return NotImplemented
+
+def findAll(str, instr, index):
+    return NotImplemented
+
+def replace(str, tostr, instr):
+    return NotImplemented
+
+def inDutchThreeDigit(digit):
+    return NotImplemented
+    
+def decompose(digit):
+    return NotImplemented
